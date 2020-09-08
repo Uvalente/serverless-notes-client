01d56796960f555f5056dccbd6faffdae1f2259d
Umberto Valente <uvalente89@gmail.com>
2020-09-08 16:50:12 +0100

    :wrench: add signup forms

---
+++ src/containers/Signup.js
@@ -0,0 +1,126 @@
+import React, { useState } from 'react'
+import { useHistory } from 'react-router-dom'
+import { Form } from "react-bootstrap";
+import LoaderButton from "../components/LoaderButton";
+import { useAppContext } from "../libs/contextLib";
+import { useFormFields } from "../libs/hookLib";
+import { onError } from "../libs/errorLib";
+import "./Signup.css";
+
+export default function Signup() {
+  const [fields, handleFieldChange] = useFormFields({
+    email: '',
+    password: '',
+    confirmPassword: '',
+    confirmationCode: ''
+  })
+  const history = useHistory()
+  const [newUser, setNewUser] = useState(null)
+  const { userHasAuthenticated } = useAppContext()
+  const [isLoading, setIsLoading] = useState(false)
+
+  function validateForm() {
+    return (
+      fields.email.length > 0 &&
+      fields.password.length > 0 &&
+      fields.password === fields.confirmPassword
+    )
+  }
+
+  function validateConfirmationForm() {
+    return fields.confirmationCode.length > 0
+  }
+
+  async function handleSubmit(e) {
+    e.preventDefault()
+
+    setIsLoading(true)
+    setNewUser('test')
+    setIsLoading(false)
+  }
+
+  async function handleConfirmationSubmit(e) {
+    e.preventDefault()
+
+    setIsLoading(true)
+  }
+
+  function renderForm() {
+    return (
+      <form onSubmit={handleSubmit}>
+        <Form.Group controlId="email">
+          <Form.Label>Email</Form.Label>
+          <Form.Control
+            size='lg'
+            autoFocus
+            type="email"
+            value={fields.email}
+            onChange={handleFieldChange}
+          />
+        </Form.Group>
+        <Form.Group controlId="password">
+          <Form.Label>Password</Form.Label>
+          <Form.Control
+            size='lg'
+            type="password"
+            value={fields.password}
+            onChange={handleFieldChange}
+          />
+        </Form.Group>
+        <Form.Group controlId="confirmPassword">
+          <Form.Label>Confirm Password</Form.Label>
+          <Form.Control
+            size='lg'
+            type="password"
+            onChange={handleFieldChange}
+            value={fields.confirmPassword}
+          />
+        </Form.Group>
+        <LoaderButton
+          block
+          type="submit"
+          isLoading={isLoading}
+          disabled={!validateForm()}
+        >
+          Signup
+        </LoaderButton>
+      </form>
+    )
+  }
+
+  function renderConfirmationForm() {
+    return (
+      <form onSubmit={handleConfirmationSubmit}>
+        <Form.Group controlId="confirmationCode">
+          <Form.Label>Confirmation Code</Form.Label>
+          <Form.Control
+            size='lg'
+            autoFocus
+            type="tel"
+            onChange={handleFieldChange}
+            value={fields.confirmationCode}
+            aria-describedby="confirmationHelpBlock"
+          />
+          <Form.Text id="confirmationHelpBlock" muted>
+            Please check your email for the code.
+          </Form.Text>
+        </Form.Group>
+        <LoaderButton
+          block
+          type="submit"
+          isLoading={isLoading}
+          disabled={!validateConfirmationForm()}
+        >
+          Verify
+        </LoaderButton>
+      </form>
+    )
+  }
+
+  return (
+    <div className="Signup">
+      {newUser === null ? renderForm() : renderConfirmationForm()}
+    </div>
+  )
+
+}
