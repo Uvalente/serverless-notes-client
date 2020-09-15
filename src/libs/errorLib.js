ea9fd7c5390e3e989b58884c1c0c203a7a9b5092
Umberto Valente <uvalente89@gmail.com>
2020-09-15 15:52:31 +0100

    :wrench: add sentry for error logging

--- src/libs/errorLib.js
+++ src/libs/errorLib.js
@@ -1,10 +1,42 @@
+import * as Sentry from "@sentry/react";
+
+const isLocal = process.env.NODE_ENV === "development";
+
+export function initSentry() {
+  if (isLocal) {
+    return;
+  }
+
+  Sentry.init({ dsn: "https://b2b7ed70c1c949e68b9861e15a88437f@o448403.ingest.sentry.io/5429733" });
+}
+
+export function logError(error, errorInfo = null) {
+  if (isLocal) {
+    return;
+  }
+
+  Sentry.withScope((scope) => {
+    errorInfo && scope.setExtras(errorInfo);
+    Sentry.captureException(error);
+  });
+}
+
+
 export function onError(error) {
-  let message = error.toString();
+  let errorInfo = {}
+  let message = error.toString()
 
   // Auth errors
   if (!(error instanceof Error) && error.message) {
-    message = error.message;
+    errorInfo = error
+    message = error.message
+    error = new Error(message)
+    // API errors
+  } else if (error.config && error.config.url) {
+    errorInfo.url = error.config.url
   }
 
-  alert(message);
+  logError(error, errorInfo)
+
+  alert(message)
 }
